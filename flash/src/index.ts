7aabc74e691125dee752610bc32ff063b7784a9e
Arthur Jacobina <129566256+Arthur-Jacobina@users.noreply.github.com>
2025-03-09 08:00:24 +0700

    fix: typo in concat

--- flash/src/index.ts
+++ flash/src/index.ts
@@ -3,7 +3,7 @@ import { HYPERBOLIC_ACTIONS } from "./actions/hyperbolic";
 import { ETHEREUM_ACTIONS } from "./actions/ethereum";
 import { EXA_ACTIONS } from "./actions/exa";
 
-export const ZAP_ACTIONS = GOOGLE_ACTIONS.concat(HYPERBOLIC_ACTIONS).concat(ETHEREUM_ACTIONS).concat(AI_ACTIONS);
+export const ZAP_ACTIONS = GOOGLE_ACTIONS.concat(HYPERBOLIC_ACTIONS).concat(ETHEREUM_ACTIONS).concat(EXA_ACTIONS);
 
 export * from "./actions/zap_action";
 
